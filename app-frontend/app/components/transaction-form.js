79c45f7e28deb05454e45c01ed2986e7e077699c
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-05-08 22:58:56 +0100

    Creates POST request that connects with transactions controller

--- app-frontend/app/components/transaction-form.js
+++ app-frontend/app/components/transaction-form.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-
+import fetch from 'fetch';
 
 export default class TransactionFormComponent extends Component {
   @service store;
@@ -15,5 +15,18 @@ export default class TransactionFormComponent extends Component {
 
     const accountId = event.target.elements['account-id'].value;
     const amount = event.target.elements.amount.value;
+
+    const url = `http://localhost:8080/api/v1/transactions`;
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        accountId,
+        amount,
+      }),
+    });
   }
 }
