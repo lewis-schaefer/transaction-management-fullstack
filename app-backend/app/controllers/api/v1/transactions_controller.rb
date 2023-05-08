8edb71bdcb61233d21725a464051f587e92b11e2
lewis schaefer <lewis.schaefer@hotmail.co.uk>
2023-05-08 23:38:48 +0100

    Creates new transaction from POST params. Attemps to solve issue with tests

--- app-backend/app/controllers/api/v1/transactions_controller.rb
+++ app-backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::TransactionsController < ApplicationController
   before_action :set_transactions, only: [:index]
+  before_action :set_account, only: [:create]
+  skip_before_action :verify_authenticity_token #solves the CSRF error
 
   def index
     render json: {
@@ -15,9 +17,52 @@ class Api::V1::TransactionsController < ApplicationController
     }
   end
 
+  def create
+    # binding.pry
+
+    transaction = Transaction.new(
+      account: @account,
+      transaction_id: SecureRandom.uuid,
+      transaction_account_id: @account.account_id,
+      amount: transaction_params[:amount]
+    )
+
+    @account.balance += transaction_params[:amount].to_f
+    @account.save!
+
+    if transaction.valid?
+      if transaction.save
+        # render json: {
+        #   data: {
+        #     transaction: {
+        #       transaction_id: transaction.transaction_id,
+        #       transaction_amount: transaction.amount,
+        #       account_id: transaction.account_id,
+        #       account_balance: transaction.account.balance,
+        #       created_at: transaction.created_at
+        #     }
+        #   }, status: :ok
+        # }
+        render json: transaction, status: :created, message: 'Transaction created'
+      else
+        render json: {
+          errors: transaction.errors.full_messages
+        }, status: :unprocessable_entity
+      end
+    end
+  end
+
   private
 
   def set_transactions
     @transactions = Transaction.all.includes(:account)
+  end
+
+  def set_account
+    @account = Account.find_by(account_id: transaction_params[:accountId])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:amount).merge(accountId: params[:accountId])
   end
 end
